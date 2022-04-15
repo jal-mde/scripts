3dbf8b7227a0bfe00402e90ff07991b78878a4c5
jal-mde <102790445+jal-mde@users.noreply.github.com>
2022-04-15 00:11:53 -0500

    Update setup_kafka.sh

--- setup_kafka.sh
+++ setup_kafka.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 
 #Download and unzip Kafka to one of the nodes
-echo "Download and unzip Kafka to one of the nodes"
+echo "Downloading Kafka..."
 wget https://downloads.apache.org/kafka/2.8.1/kafka_2.13-2.8.1.tgz
 tar -xzf kafka_2.13-2.8.1.tgz
-cd kafka_2.13-2.8.1
 #Start zookeeper
-echo "Starting Zookeeper"
-bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-#Start Kafka
-echo "Starting Kafka"
-bin/kafka-server-start.sh -daemon config/server.properties
+echo "Starting Zookeeper..."
+kafka_2.13-2.8.1/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+#Starting Kafka
+echo "Starting Kafka..."
+kafka_2.13-2.8.1/bin/kafka-server-start.sh -daemon config/server.properties
 #Create a Kafka topic named "test"
 echo "Creating a Kafka topic named test"
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
-echo "Now you are set to go!"
+kafka_2.13-2.8.1/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic project
+echo "Now you are set to go! The Topic project has been created"
