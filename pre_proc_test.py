9973f453b5a96a8f42f20e18c24af9b73ddea35d
jal-mde <102790445+jal-mde@users.noreply.github.com>
2022-04-29 18:16:48 -0500

    Create pre_proc_test.py

---
+++ pre_proc_test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sklearn
+from sklearn.preprocessing import OrdinalEncoder
+
+df_reviews = pd.read_csv("./Webpages_Classification_test_data.csv")
+df_reviews = df_reviews[["url_len", "geo_loc", "tld", "who_is", "https", "label"]]
+
+df_reviews['geo_loc'] = OrdinalEncoder().fit_transform(df_reviews.geo_loc.values.reshape(-1,1))
+df_reviews['tld'] = OrdinalEncoder().fit_transform(df_reviews.tld.values.reshape(-1,1))
+df_reviews['who_is'] = OrdinalEncoder().fit_transform(df_reviews.who_is.values.reshape(-1,1))
+df_reviews['https'] = OrdinalEncoder().fit_transform(df_reviews.https.values.reshape(-1,1))
+df_reviews['label'] = OrdinalEncoder().fit_transform(df_reviews.label.values.reshape(-1,1))
+
+df_reviews['geo_loc'] = "[" + df_reviews['geo_loc']
+df_reviews['label'] = df_reviews['label'] + "]"
+
+df_reviews = df_reviews.to_csv("prec_data_final.csv", index=False)
+#,url,url_len,ip_add,geo_loc,tld,who_is,https,js_len,js_obf_len,content,label
